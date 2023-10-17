2d13202df2873ceff05d887cc9516bf0b06ab6ed
EderHodaj <eder0302@gmail.com>
2023-10-17 02:26:39 -0700

    4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letters = 'a';
+
+	while (letters <= 'z')
+	{
+		if (letters != 'e' && letters != 'q')
+		{
+			putchar(letters);
+		}
+		letters++;
+
+	}
+	putchar ('\n');
+	return (0);
+}
