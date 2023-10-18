8677d6ec6c9e99ee6986f8b269d8f231104b6e29
EderHodaj <eder0302@gmail.com>
2023-10-18 12:23:11 -0700

    7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,22 @@
 #include "main.h"
+/**
+ * print_last_digit - Entry point
+ * @a: is parameter
+ * return abs(a);
+ * Return:0
+ */
 
 int print_last_digit(int a)
 {
 	if (a > 0)
+	{
 		_putchar(a % 10 + '0');
-	else if (a == 0)
-		_putchar(a % 10 + '0');
+		return (a % 10);
+	}
 	else
+	{
+		a = -a;
 		_putchar(a % 10 + '0');
-
-	return (a % 10);
+		return (a % 10);
+	}
 }
