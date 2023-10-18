c433c5d6f18a8e27908755d61df6d9c0ff7d1006
EderHodaj <eder0302@gmail.com>
2023-10-18 03:02:26 -0700

    2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int number;
+
+	for (number = 0; number <10; number++)
+{
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar ('\n');
+}
+}
