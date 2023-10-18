6d9b58120156093e3b3547a919e819d8129cbaf1
EderHodaj <eder0302@gmail.com>
2023-10-18 12:25:34 -0700

    7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,14 +8,14 @@
 
 int print_last_digit(int a)
 {
-	if (a > 0)
+	if (a < 0)
 	{
+		a = -a;
 		_putchar(a % 10 + '0');
 		return (a % 10);
 	}
 	else
 	{
-		a = -a;
 		_putchar(a % 10 + '0');
 		return (a % 10);
 	}
