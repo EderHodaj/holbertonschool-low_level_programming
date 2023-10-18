62aefb1633bf99df5d58a56349b330ed2d84f997
EderHodaj <eder0302@gmail.com>
2023-10-18 03:06:56 -0700

    2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+/** 
+ * * print_alphabet_x10 - Entry point 
+ * Return:0
  */
 
 void print_alphabet_x10(void)
@@ -11,7 +10,7 @@ void print_alphabet_x10(void)
 	char letter;
 	int number;
 
-	for (number = 0; number <10; number++)
+	for (number = 0; number < 10; number++)
 {
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
