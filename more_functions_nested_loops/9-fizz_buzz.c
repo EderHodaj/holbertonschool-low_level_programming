c6479152e7464e45aeb6b5209d3a6a8d56507cd1
EderHodaj <eder0302@gmail.com>
2023-10-20 04:49:33 -0700

    9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+	int i;
+
+	for (i = 1; 1 <= 100; i++)
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	return (0);
+}
