8a6d93faffbb73701d62e7edd61614465c707a98
EderHodaj <eder0302@gmail.com>
2023-10-23 04:41:39 -0700

    4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - exx4
+ * @s:variable
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int length, last_index;
+
+	while (s[i] != '\0')
+	{
+		i++;
+		last_index = i - 1;
+	}
+	for (length = last_index; length >= 0; length--)
+		_putchar (s[length]);
+	_putchar ('\n');
+}
