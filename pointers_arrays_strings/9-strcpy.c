63f0783e39aab54484e674495d90c81fded9c682
EderHodaj <eder0302@gmail.com>
2023-10-24 03:05:32 -0700

    9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_strcpy - exx9
+ * @dest:sdfs
+ * @src:sdf
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i++] = '\0';
+	return (dest);
+}
