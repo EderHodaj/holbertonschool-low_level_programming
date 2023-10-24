5758756aba03493d06618d0796b8dee68d943226
EderHodaj <eder0302@gmail.com>
2023-10-24 01:57:58 -0700

    7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -3,7 +3,7 @@
  * puts_half - 7exx
  * @str: pointers
  */
- 
+
 void puts_half(char *str)
 {
 	int i = 0, len, mid, j;
@@ -14,11 +14,13 @@ void puts_half(char *str)
 	}
 	len = i;
 	mid = len / 2;
-	if (len % 2 == 1){
+	if (len % 2 == 1)
+	{
 		for (j = mid + 1; j < len; j++)
 			_putchar (str[j]);
 	}
-	else {
+	else
+	{
 		for (j = mid; j < len; j++)
 		{
 			_putchar (str[j]);
