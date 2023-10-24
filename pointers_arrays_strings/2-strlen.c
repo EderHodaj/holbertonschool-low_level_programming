3a5f73ace15de5cf6607eae3454e23faaff3e371
EderHodaj <eder0302@gmail.com>
2023-10-24 01:38:59 -0700

    7

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -5,14 +5,18 @@
  * Return: `value
  */
 
-int _strlen(char *s)
+void puts_half(char *str)
 {
-	int i = 0;
-
-		while (s[i] != '\0')
-		{
-			i++;
-		}
-
-	return (i);
+	int i = 0, len, mid, j;
+	
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	len = i;
+	mid = len / 2;
+	for (j = mid; j < len; j++)
+	{
+		_putchar(str[j]);
+	}
 }
---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * puts_half - 7exx
+ * @str: pointers
+ */
+ 
+void puts_half(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+}
