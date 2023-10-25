ebaebbef83e03c4f5d5226cdee2cdb8b40393349
EderHodaj <eder0302@gmail.com>
2023-10-25 03:51:14 -0700

    3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+int _strcmp(char *s1, char *s2)
+{
+	int diff = 0;
+	int i = 0;
+
+	while (s1[i] != '\0' || s2[i])
+		if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
+	return (diff);
+}
