cceacb6b2c951bd50c4f61d8426d83297a760fe6
EderHodaj <eder0302@gmail.com>
2023-10-25 03:58:03 -0700

    3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,20 +1,25 @@
 #include "main.h"
+
 /**
- * _strcmp - sdfsd
- * @s1:sdfsd
- * @s2:dsvsd
+ *_strcmp - ygujh
+ *@s1: seff
+ *@s2: sdyu
+ *Return: sedfa
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int diff = 0;
 	int i = 0;
+	int diff = 0;
 
 	while (s1[i] != '\0' || s2[i] != '\0')
+	{
 		if (s1[i] != s2[i])
 		{
 			diff = s1[i] - s2[i];
 			break;
 		}
+		i++;
+	}
 	return (diff);
 }
