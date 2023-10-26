4d17b0184c14a37c0f2ec02e65d7670d09d3206d
EderHodaj <eder0302@gmail.com>
2023-10-26 08:43:31 -0700

    2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,19 +1,22 @@
 #include <stddef.h>
 /**
- * _strchr - dsfsdf
- * @s:dsfgsdg
- * @c:sdfs
- * Return:dfsf
+ * _strchr - functions to search a char in str
+ * @s: pointer to string
+ * @c: character to fill
+ * Return: string
  */
 
 char *_strchr(char *s, char c)
 {
-	int i;
+	unsigned int i;
+
+	if (c == '\0')
+		return ("");
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
-		return (s + i);
+			return (s + i);
 	}
 	return (NULL);
 }
