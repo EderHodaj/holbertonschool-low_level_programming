f4e4b1051e5e1edc2b26bd915ff7a292fddfcf65
EderHodaj <eder0302@gmail.com>
2023-10-30 06:12:32 -0700

    3

---
+++ recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * factorial - sdfsf
+ * @n:sdfsdf
+ * Return:sdfsdf
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n > 0)
+		return (n * factorial(n - 1));
+	else 
+		return (1);
+}
