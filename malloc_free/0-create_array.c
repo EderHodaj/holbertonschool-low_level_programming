2c356a2ca58851d9467c768797232c2a82aa2a06
EderHodaj <eder0302@gmail.com>
2023-11-06 02:02:16 -0800

    0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - sdfsdf
+ * @size:sdgsdg
+ * @c:sgsdgd
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i = 0;
+	char *array = (char *)malloc(size * sizeof(char));
+
+	if (size == 0)
+	{
+	return NULL;
+	}
+
+	if (array == NULL)
+	{
+	return NULL;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+
+    return array;
+}
