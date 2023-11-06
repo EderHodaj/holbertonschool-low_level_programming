f5b38391a7d215e131a7122d21d156023a533979
EderHodaj <eder0302@gmail.com>
2023-11-06 09:15:19 -0800

    2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,49 +1,28 @@
-#include "main.h"
 #include <stdlib.h>
+#include <string.h>
 /**
- * str_concat - sdfsdf
- * @si:sdfgsdf
- * @s2:sdgsg
- * Return:dfsd
+ * str_concat - function to concate 2 arry
+ * @s1: first string
+ * @s2: second string
+ * Return: null or a pointer to s
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	int l1, l2, i;
-	char* concat;
-	
+	unsigned int i;
+	char *s;
+
 	if (s1 == NULL)
-		return (NULL);
-
-	l1 = 0;
-	while (s1[l1] != '\0')
-	{
-		l1++;
-	}
-
+		s1 = "";
 	if (s2 == NULL)
-                return (NULL);
+		s2 = "";
 
-	l2 = 0;
-	while (s2[l2] != '\0')
-	{
-		l2++;
-	}
-
-	concat = (char*)malloc(l1 + l2 + 1);
-	
-	if (concat == NULL)
+	s = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
+	if (s == NULL)
 		return (NULL);
-
-	for (i = 0; i < l1; i++)
-	{
-		concat[i] = s1[i];
-	}
-	for (i = 0; i < l2; i++)
-	{
-		concat[l1 + i] = s2[i];
-	}
-	concat[l1 + l2] = '\0';
-
-	return (concat);
+	for (i = 0; i < strlen(s1); i++)
+		s[i] = s1[i];
+	for (i = 0; i < strlen(s2); i++)
+		s[strlen(s1) + i] = s2[i];
+	s[strlen(s1) + i] = '\0';
+	return (s);
 }
