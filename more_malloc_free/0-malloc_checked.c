68701d1be5e271af0567a9d4d9593d6c0fc78f56
EderHodaj <eder0302@gmail.com>
2023-11-07 04:46:47 -0800

    0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - sdfsdf
+ * @b:dsfsfsdf
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned int* ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit (98);
+
+	return (ptr);
+
+}
