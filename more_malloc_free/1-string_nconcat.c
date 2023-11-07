1e5986a7ff527880094170604c39b01cc0786fb7
EderHodaj <eder0302@gmail.com>
2023-11-07 10:51:33 -0800

    1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -17,9 +17,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int total_len;
 
 	if (s1 == NULL)
-		return ("");
+		s1 = ("");
 	if (s2 == NULL)
-		return ("");
+		s2 = ("");
 
 	while (s1[length1] != '\0')
 		length1++;
