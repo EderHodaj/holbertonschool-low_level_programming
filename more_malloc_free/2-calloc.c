5e419028ad34990a681f7d927f07456e0a2dcb11
EderHodaj <eder0302@gmail.com>
2023-11-08 02:44:22 -0800

    2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,13 +10,16 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int *ptr;
-
+	unsigned int i;
 	ptr = malloc(size*nmemb);
 
 	if (ptr == NULL)
 		return (NULL);
 	if (nmemb == 0 || size == 0)
 		return (NULL);
+
+	for (i = 0; i < (nmemb*size); i++)
+		ptr[i] = 0;
 	return (ptr);
 
 }
