ca922b5a2a38395a007226d4713b5a77c2ce56c6
EderHodaj <eder0302@gmail.com>
2023-11-13 01:26:57 -0800

    0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - Short description
+ * @name:sdfsd
+ * @age:sdfsdf
+ * @owner:sdfsdf
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+	name, type = char *
+	age, type = float
+	owner, type = char *
+};
