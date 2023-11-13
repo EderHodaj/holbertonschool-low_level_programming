59d346c3c611bce3f6264fb076ee49c1c1679e95
EderHodaj <eder0302@gmail.com>
2023-11-13 11:17:19 -0800

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,10 +14,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	
 	my_dog = malloc(sizeof(dog_t));
 	if (my_dog == NULL)
-		free;
-	my_dog-> name = name;
-	my_dog-> age = age;
-	my_dog-> owner =owner;
+		return (NULL);
+	my_dog->name = name;
+	my_dog->age = age;
+	my_dog->owner =owner;
 
-	return (my_dog);
+	free (my_dog);
 }
