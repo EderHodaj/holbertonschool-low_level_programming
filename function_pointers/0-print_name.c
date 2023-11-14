65b41c7a80825d5b6631421c83928943907de608
EderHodaj <eder0302@gmail.com>
2023-11-14 09:22:20 -0800

    0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - sdfgsdf
+ * @name:sdfsdf
+ * @f:xxcvxcv
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
