73e6b7328a72a8fbd88582d52003d85bcb53d82a
EderHodaj <eder0302@gmail.com>
2023-11-14 13:48:51 -0800

    2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,18 +1,25 @@
+#include <stdio.h>
 #include "function_pointers.h"
+
 /**
- * int_index - sdfsdf
- * @array:sfsdfs
- * @size:sfsd
- * @cmp:sdfsdf
+ * int_index - function that search for an integer
+ * @array: array to search in
+ * @size: number of elements
+ * @cmp: pointer to the function for compare value
+ * Return: index of the first element where cmp function
+ * does not return 0, and -1 where no element matches or
+ * size is negative
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (cmp == NULL || array == NULL)
+		return (-1);
 	if (size <= 0)
 		return (-1);
-	for (i = 0; i < (size-1); i++)
+	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
 			return (i);
