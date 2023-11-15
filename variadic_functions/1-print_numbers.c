7347d4330f433789abbbf95bc2d7ec3b4da32d33
EderHodaj <eder0302@gmail.com>
2023-11-15 10:32:10 -0800

    1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -15,14 +15,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
-	if (separator == NULL)
-		return;
 	for (i = 0; i < n; i++)
 	{
 		int x = va_arg(args, int);
 		printf("%d", x);
 
-		if (i < n - 1)
+		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}
 	printf("\n");
