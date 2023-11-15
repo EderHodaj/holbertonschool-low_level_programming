15a08add026097e8826cb355d07da1ee080d92cf
EderHodaj <eder0302@gmail.com>
2023-11-15 09:39:45 -0800

    0

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - cxvxcv
+ * @n:sfsdf
+ * Return:sdfsd
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	unsigned int s = 0;
+	va_list args;
+	va_start(args, n);
+
+	if (n == 0)
+                return (0);
+
+	for (i = 0; i < n; i++)
+	{
+		unsigned int x = va_arg(args, unsigned int);
+		s = s + x;
+	}
+	va_end(args);
+	return (s);
+}
