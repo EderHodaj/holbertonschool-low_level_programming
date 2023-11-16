d58803bfa691d7bbc977d9cc88538f2668e0f64c
EderHodaj <eder0302@gmail.com>
2023-11-16 11:42:12 -0800

    3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -37,8 +37,7 @@ void print_all(const char * const format, ...)
                 str = va_arg(args, char *);
                 if (str == NULL)
                     printf("(nil)");
-                else
-                    printf("%s", str);
+		printf("%s", str);
                 break;
         }
 
