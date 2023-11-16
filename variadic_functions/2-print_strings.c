497daaf0d6787fae4ef2643eeccd42bba36720ec
EderHodaj <eder0302@gmail.com>
2023-11-16 01:09:53 -0800

    2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 		if (x == NULL)
-			printf("nil");
+			printf("(nil)");
 	}
 	printf("\n");
 	va_end(args);
