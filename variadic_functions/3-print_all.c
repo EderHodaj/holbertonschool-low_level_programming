de9441273a3847a107a1f3888d33189f4e8cd53f
EderHodaj <eder0302@gmail.com>
2023-11-16 11:40:29 -0800

    3

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,53 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_all - Print anything based on a format string.
+ * @format: Format string containing format specifiers.
+ */
+void print_all(const char * const format, ...)
+{
+    va_list args;
+    int i = 0;
+    char *str;
+    int num;
+    float flt;
+    char c;
+
+    va_start(args, format);
+
+    while (format && format[i])
+    {
+        switch (format[i])
+        {
+            case 'c':
+                c = va_arg(args, int);
+                printf("%c", c);
+                break;
+            case 'i':
+                num = va_arg(args, int);
+                printf("%d", num);
+                break;
+            case 'f':
+                flt = va_arg(args, double);
+                printf("%f", flt);
+                break;
+            case 's':
+                str = va_arg(args, char *);
+                if (str == NULL)
+                    printf("(nil)");
+                else
+                    printf("%s", str);
+                break;
+        }
+
+        if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's') && format[i + 1] != '\0')
+            printf(", ");
+
+        i++;
+    }
+
+    printf("\n");
+    va_end(args);
+}
