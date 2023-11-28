eaec5391f7d8cfb4f03e3d97f51f4f9ac87e480c
EderHodaj <eder0302@gmail.com>
2023-11-28 06:11:40 -0800

    2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * add_node - check the code
+ * @head:sdfsdf
+ * @str:sdfsdf
+ * Return: Always 0.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+	
+	if (str == NULL)
+		return (NULL);
+
+	if (new == NULL)
+		exit (EXIT_FAILURE);
+
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free (new);
+		exit(EXIT_FAILURE);
+	}
+	new->len = strlen(str);
+
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
