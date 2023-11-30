82cd4521139f807527388963f8d7e65ea863e1f9
EderHodaj <eder0302@gmail.com>
2023-11-30 07:33:37 -0800

    3

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**a
+ * add_dnodeint_end - sdfsd
+ * @head:sfsdf
+ * @n:dsgsdf
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *temp, *tp;
+	temp = malloc(sizeof(dlistint_t));
+	
+	if (temp == NULL)
+		return (NULL);
+
+	temp->next = NULL;
+	temp->n = n;
+	tp = *head;
+
+	while (tp != NULL)
+		tp = tp->next;
+	tp->next = temp;
+	temp->prev = tp;
+
+	return (temp);
+}
