fe7a9678d4e5f095ab2f7544f13f201d1f911276
EderHodaj <eder0302@gmail.com>
2023-11-30 07:57:03 -0800

    4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint - sdfsd
+ * @head:sfsdf
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current, *next;
+
+	current = head;
+
+	while (current != NULL)
+		next = current->next;
+	free(current);
+	current = next;
+}
